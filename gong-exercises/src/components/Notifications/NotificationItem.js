fe0b7a212d93db0f476ea92ffb0e21ec6c1c39e3
Haggai Meltzer <haggai.meltzer@gong.io>
2020-04-13 15:58:33 +0300

    notifications continue

--- gong-exercises/src/components/Notifications/NotificationItem.js
+++ gong-exercises/src/components/Notifications/NotificationItem.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import '../../css/notif_item.css';
 
 export default function NotificationItem(props){
     const {notificationData} = props;
        
     return (
         <div className="notification-item">
-            <img className="notification-img" alt="notification-img" src={notificationData.avatar_img}/>
+            <div className="notification-avatars">
+            {
+                notificationData.avatar_imgs.map((avatar_img)=> 
+                <img className="notification-img" alt="notification-img" src={avatar_img}/>
+                )
+                
+            }
+            </div>
             <span className="notification-title">{notificationData.title}</span>
             <span className="notification-text">{notificationData.text}</span>
         </div>
